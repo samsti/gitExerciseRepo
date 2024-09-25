a6de55e653496582ec4ea9a6f7cced04d499bfe8
Samuel <114352994+samsti@users.noreply.github.com>
2024-09-25 10:57:14 +0200

    change1

--- demo1/src/main/java/dk/easv/demo1/HelloApplication.java
+++ demo1/src/main/java/dk/easv/demo1/HelloApplication.java
@@ -15,6 +15,8 @@ public class HelloApplication extends Application {
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
+
+        //change for GitHub
     }
 
     public static void main(String[] args) {
